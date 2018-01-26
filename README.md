675be5fd2f134c0b30e273665a6cc85f29c9c24f
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-26 14:34:20 -0500

    complete assignement

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+![Bitmaker](https://github.com/johncarlolopez/bitmaker-reference/blob/master/bitmakerlogo.svg)
 [See assignment in Alexa](https://alexa.bitmaker.co/wdi/may-2017/assignments/2591/latest)
+
+# How to Test
+### Wednesday, Jun 21st
+
+##Excercise
+___
+Fork and clone this repository.  
+
+Try running ruby test_robot.rb. If all is well, you should see 9 skipped tests and no error messages.
+Read the code in robot.rb and the descriptions of the unit test to be implemented in test_robot.rb.
+One by one, remove skip from each unit test and in its place write your test code. In the end your test output should read
+```
+9 runs, 9 assertions, 0 failures, 0 errors, 0 skips
+```
--- test_robot.rb
+++ test_robot.rb
@@ -5,75 +5,91 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
     # arrange
-
+    test_bot = Robot.new
+    test_bot.foreign_model = 'Toyota'
+    test_bot.needs_repairs = true
     # act
-
+    actual_value = test_bot.station
+    expected_value = 1
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
-
+    test_bot = Robot.new
+    test_bot.vintage_model = true
+    test_bot.needs_repairs = true
     # act
-
+    actual_value = test_bot.station
+    expected_value = 2
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
-
+    test_bot = Robot.new
+    test_bot.needs_repairs = true
     # act
-
+    actual_value = test_bot.station
+    expected_value = 3
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
     # arrange
-
+    test_bot = Robot.new
     # act
-
+    actual_value = test_bot.station
+    expected_value = 4
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
     # arrange
-
+    test_bot = Robot.new
     # act
-
+    actual_value = test_bot.prioritize_tasks
+    expected_value = -1
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
     # arrange
-
+    test_bot = Robot.new
+    test_bot.todos = [1,2,3,4]
     # act
-
+    actual_value = test_bot.prioritize_tasks
+    expected_value = 4
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_workday_on_day_off_returns_false
-    skip
     # arrange
-
+    test_bot = Robot.new
+    test_bot.todos = [1,2,3,4]
     # act
-
+    actual_value = test_bot.prioritize_tasks
+    expected_value = 4
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_workday_not_day_off_returns_true
-    skip
     # arrange
-
+    test_bot = Robot.new
+    test_bot.day_off = (Time.now + 60 * 60 * 24).day
     # act
-
+    actual_value = test_bot.workday?(Time.now.day)
+    expected_value = true
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
 end
